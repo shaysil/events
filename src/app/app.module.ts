110154b966e4d352ce5778f2b5786919a8b6d70d
Shay Silverstein <shay_sil@jer.ad.malam.com>
2018-06-27 15:24:13 +0300

    before session 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,23 @@ import { NgModule } from '@angular/core';
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventsThumbnailComponent } from './events/event-thumbnail.component';
-import {NavBarComponent} from './nav/navbar.component'
+import { NavBarComponent } from './nav/navbar.component';
+import { EventService } from './events/shared/event.service';
+import {ToastrService} from './common/toastr.service';
+
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+
   ],
   declarations: [
     EventsAppComponent,
     EventsListComponent,
     EventsThumbnailComponent,
     NavBarComponent
+
   ],
-  
+  providers: [EventService,ToastrService],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
